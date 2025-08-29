92882a18ac5c7bd52fbfde5069f945b3062f09b1
Khadiii1811 <toankhoa86443@gmail.com>
2025-08-29 19:45:39 +0700

    third commit to config

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,11 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className="text-3xl font-bold underline ">Hello Tailwind</h1>
+    <div className="flex gap-3">
+    <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+    Primary
+  </button>
+  </div>
     <App />
   </StrictMode>,
 )
