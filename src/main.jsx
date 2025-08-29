384ba6c2c9bf59b545740c5c46ff4893c7969b82
Khadiii1811 <toankhoa86443@gmail.com>
2025-08-29 09:45:53 +0700

    done config

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h1 className="text-3xl font-bold ">Hello Tailwind</h1>
     <App />
   </StrictMode>,
 )
